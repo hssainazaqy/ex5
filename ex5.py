14146e12acff5022f56dbc5a436e0d7e54a87c23
hssainazaqy <hssain.azaqy@campus.technion.ac.il>
2022-06-25 19:21:28 +0300

    update 1 and the last one- tests PASSED

--- ex5.py
+++ ex5.py
@@ -12,7 +12,14 @@ def names_of_registered_students(input_json_path, course_name):
     :param course_name: The name of the course.
     :return: List of the names of the students.
     """
-    pass
+    return_list = []
+    with open(input_json_path) as f:
+        data = json.load(f)
+    for id,student in data.items():
+        if course_name in student['registered_courses']:
+            return_list.append(student['student_name'])
+
+    return return_list
 
 
 def enrollment_numbers(input_json_path, output_file_path):
@@ -23,7 +30,25 @@ def enrollment_numbers(input_json_path, output_file_path):
     :param input_json_path: Path of the students database json file.
     :param output_file_path: Path of the output text file.
     """
-    pass
+    with open(input_json_path) as f:
+        data = json.load(f)
+    new_dict = {}
+    for k in data.keys():
+        for course_name in data[k]['registered_courses']:
+            if course_name in new_dict.keys():
+                new_dict[course_name] = new_dict[course_name] + 1
+            else:
+                new_dict[course_name] = 1
+    with open(output_file_path, 'w') as g:
+
+        for course_name in sorted(new_dict):
+            g.write('"')
+            g.write(course_name)
+            g.write('"')
+            g.write(" ")
+            g.write(str(new_dict[course_name]))
+            g.write('\n')
+
 
 
 
@@ -34,7 +59,26 @@ def courses_for_lecturers(json_directory_path, output_json_path):
     :param json_directory_path: Path of the semsters_data files.
     :param output_json_path: Path of the output json file.
     """
-    pass
+    lecturers_dict = {}
+    directory = json_directory_path
+
+    for filename in os.listdir(directory):
+        curr_file_path = os.path.join(directory, filename)
+        if os.path.isfile(curr_file_path):
+            if curr_file_path.endswith('json'):
+                with open(curr_file_path) as f_path :
+
+                    data = json.load(f_path)
+                    for course_id in data.keys():
+
+                        for lecturer in data[course_id]['lecturers']:
+
+                            if lecturer in lecturers_dict.keys():
+                                if data[course_id]['course_name'] not in lecturers_dict[lecturer]:
+                                    lecturers_dict[lecturer].append(data[course_id]['course_name'])
+                            else:
+                                lecturers_dict[lecturer] = [data[course_id]['course_name']]
 
 
-
+    with open(output_json_path,'w') as outPath:
+        json.dump(lecturers_dict,outPath,indent=4)
